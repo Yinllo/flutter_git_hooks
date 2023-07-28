6f7f5f84c07cd8eb440b08ed8946e789ad84a3f2
殷亮 <yinliang@wshifu.com>
2023-07-28 15:44:10 +0800

    <feat>[]<测试>

--- bin/preCommit.dart
+++ bin/preCommit.dart
@@ -2,5 +2,5 @@ import 'package:lint_plugin/lint_plugin.dart';
 
 void main(List arguments) {
   print("开始获取到的参数是：" + arguments[0]);
-  FlutterGitHooksPlugin.gitHooks(arguments);
+  FlutterGitHooksPlugin.preCommit();
 }
