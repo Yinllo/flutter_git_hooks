df9d1fb4734fec7b374761c996bcf15127cf879d
殷亮 <yinliang@wshifu.com>
2023-07-28 14:37:04 +0800

    <feat>[]<测试>

--- lib/lint_plugin.dart
+++ lib/lint_plugin.dart
@@ -56,13 +56,13 @@ class FlutterGitHooksPlugin {
   }
 
   static Future<bool> commitMsg() async {
-    // var commitMsg = Utils.getCommitEditMsg();
-    // if (commitMsg.startsWith('fix:')) {
-    //   return true; // you can return true let commit go
-    // } else {
-    //   print('you should add `fix` in the commit message');
-    //   return false;
-    // }
+    var commitMsg = Utils.getCommitEditMsg();
+    if (commitMsg.startsWith('fix:')) {
+      return true; // you can return true let commit go
+    } else {
+      print('you should add `fix` in the commit message');
+      return false;
+    }
     return true;
   }
 }
